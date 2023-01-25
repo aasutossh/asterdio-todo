9fa87b3d3174e7b0ac107cee199e167d25dd050f
Jha Aasutosh <aasutossh@gmail.com>
2023-01-25 20:32:37 +0545

    fix(todo): default todo completed status to false

--- db/models/todo.js
+++ db/models/todo.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema(
   {
     text: { type: String, required: true, trim: true },
-    completed: { type: Boolean, default: true },
+    completed: { type: Boolean, default: false },
   },
   { timestamps: true }
-)
+);
 
-const Todo = mongoose.model('Todo', TodoSchema)
+const Todo = mongoose.model("Todo", TodoSchema);
 
-module.exports = Todo
+module.exports = Todo;
