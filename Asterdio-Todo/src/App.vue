bd2119e90d2bfe3f46cfd60ebf5ab9b78d46bfac
Jha Aasutosh <aasutossh@gmail.com>
2023-01-26 08:42:00 +0545

    feat: change style of completed todo, some prettier updates

--- Asterdio-Todo/src/App.vue
+++ Asterdio-Todo/src/App.vue
@@ -6,7 +6,9 @@ import ToDo from "./components/ToDo.vue";
   <div id="app bg-gray-200 min-h-screen ">
     <div class="container mx-auto">
       <header class="py-8">
-        <div class="text-center text-4xl font-black text-slate-600 underline">To-Do app</div>
+        <div class="text-center text-4xl font-black text-slate-600 underline">
+          To-Do app
+        </div>
       </header>
 
       <main>
