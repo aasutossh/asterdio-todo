bb24100cd17e24082ef98cbe579e2d94539ae134
Aasutosh Jha <aasutossh@gmail.com>
2023-01-26 08:47:49 +0545

    Meta & Style updates (#1)
    * feat: change style of completed todo, some prettier updates
    
    * fix(style): background color update

--- Asterdio-Todo/src/components/ToDo.vue
+++ Asterdio-Todo/src/components/ToDo.vue
@@ -1,50 +1,86 @@
 <template>
   <div class="mx-auto max-w-lg">
-    <div v-if="loading" class="p-4 rounded-lg shadow-md">loading...</div>
+    <div v-if="loading" class="rounded-lg p-4 shadow-md">loading...</div>
     <div v-else>
       <div class="mb-12">
-        <button class="rounded-lg bg-blue-500 p-2 text-white hover:bg-blue-600" @click="addTask">
+        <button
+          class="rounded-lg bg-blue-500 p-2 text-white hover:bg-blue-600"
+          @click="addTask"
+        >
           Add new task
         </button>
       </div>
       <ul v-if="tasks.length > 0" class="grid list-none grid-cols-1 gap-3">
-        <li v-for="(task, index) in tasks" :key="index" class="rounded-lg bg-white p-4 shadow-md">
+        <li
+          v-for="(task, index) in tasks"
+          :key="index"
+          class="rounded-lg p-4 shadow-md"
+          :class="[task.completed ? 'bg-gray-100' : 'bg-white']"
+        >
           <div v-if="index === editing" class="flex justify-between">
-
             <div class="flex items-center gap-1">
-              <input type="checkbox" v-model="task.completed"
-                class=" text-blue-500 focus:ring-blue-500 border-gray-300 rounded" @change="toggleCompleted(index)">
-              <input v-model="task.text" class="rounded-lg bg-gray-200 p-2" @keyup.enter="saveTask(index)" />
+              <input
+                type="checkbox"
+                v-model="task.completed"
+                class="rounded border-gray-300 text-blue-500 focus:ring-blue-500"
+                @change="toggleCompleted(index)"
+              />
+              <input
+                v-model="task.text"
+                class="rounded-lg bg-gray-200 p-2"
+                @keyup.enter="saveTask(index)"
+              />
             </div>
             <div class="flex gap-2">
-              <button class="rounded-lg bg-green-500 p-2 text-white hover:bg-green-600" @click="saveTask(index)">
+              <button
+                class="rounded-lg bg-green-500 p-2 text-white hover:bg-green-600"
+                @click="saveTask(index)"
+              >
                 Save
               </button>
-              <button class="rounded-lg bg-red-500 p-2 text-white hover:bg-red-600" @click="cancelEdit(index)">
+              <button
+                class="rounded-lg bg-red-500 p-2 text-white hover:bg-red-600"
+                @click="cancelEdit(index)"
+              >
                 Cancel
               </button>
             </div>
           </div>
           <div v-else class="flex justify-between">
             <div class="flex items-center gap-1">
-              <input type="checkbox" v-model="task.completed"
-                class=" text-blue-500 focus:ring-blue-500 border-gray-300 rounded" @change="toggleCompleted(index)">
-              <p class="p-2 text-gray-700" :class="[task.completed ? 'line-through' : '']">
+              <input
+                type="checkbox"
+                v-model="task.completed"
+                class="rounded border-gray-300 text-blue-500 focus:ring-blue-500"
+                @change="toggleCompleted(index)"
+              />
+              <p
+                class="p-2 text-gray-700"
+                :class="[task.completed ? 'line-through' : '']"
+              >
                 {{ task.text }}
               </p>
             </div>
             <div class="flex gap-2">
-              <button class="rounded-lg bg-blue-500 p-2 text-white hover:bg-blue-600" @click="editTask(index)">
+              <button
+                class="rounded-lg bg-blue-500 p-2 text-white hover:bg-blue-600"
+                @click="editTask(index)"
+              >
                 Edit
               </button>
-              <button class="rounded-lg bg-red-500 p-2 text-white hover:bg-red-600" @click="deleteTask(index)">
+              <button
+                class="rounded-lg bg-red-500 p-2 text-white hover:bg-red-600"
+                @click="deleteTask(index)"
+              >
                 Delete
               </button>
             </div>
           </div>
         </li>
       </ul>
-      <div v-else class="rounded-lg p-4 shadow-md">No tasks yet. Add a new one.</div>
+      <div v-else class="rounded-lg p-4 shadow-md">
+        No tasks yet. Add a new one.
+      </div>
     </div>
   </div>
 </template>
